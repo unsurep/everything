d971744c68af47326a2848c764cf0825922076c8
Swings0 <christianemmanuel641@gmail.com>
2024-08-16 03:18:51 -0700

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,8 @@ const Footer=()=>{
 
     return(
         <div>
-            <h1>Footer</h1>
+            <h1>This is footer</h1>
+            <button className="bg-black text-white pt-3 px-5 rounded-xl hover:bg-blue-800">Class</button>
         </div>
     )
 
